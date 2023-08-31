84f968714147fa58184fd946ec2f84196c3c1565
Darknab <darknab@live.fr>
2023-08-31 20:21:45 +0100

    Nest friendship_requests routes under users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   get 'friends/index'
   get 'friends/destroy'
-  resources :fiendship_requests
+  
   devise_for :users
 
-  resources :users, only: [:edit, :update, :show]
+  resources :users, only: [:edit, :update, :show] do
+    resources :friendship_requests
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
