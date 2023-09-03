c571bcaa92461252b1223db389f048605b079714
Darknab <darknab@live.fr>
2023-09-03 18:54:06 +0100

    Add a scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,13 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
   
   has_many :friendship_requests, dependent: :destroy
-  has_many :pending_friends, through: :friendship_requests, source: :friend
+  has_many :pending_friendships, through: :friendship_requests, source: :friend
 
   has_many :friendships, dependent: :destroy
   has_many :friends, through: :friendships
+
+
+  scope :not_friends, -> (user) { where.not(id: user.friends).where.not(id: user.friendship_requests).where.not(id: user.pending_friendships).where.not(id: user) }
 
   def remove_friend(friend)
     current_user.friends.destroy(friend)
