4187e0dd91562229d9be69b691e7c6c167634a8b
Darknab <darknab@live.fr>
2023-09-03 11:21:18 +0100

    Renamed friends controller to friendships controller

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class FriendsControllerTest < ActionDispatch::IntegrationTest
+class FriendshipsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get friends_index_url
     assert_response :success
