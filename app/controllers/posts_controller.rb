04234a60b27c85f4f2cfbc656d8746b35c23df65
Darknab <darknab@live.fr>
2023-09-06 19:17:57 +0100

    Fix typos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-  before_action :set_post only: [:show, :edit, :update, :destroy]
-  before_action :authorise_user only: [:edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorise_user, only: [:edit, :update, :destroy]
 
   def new
     @post = current_user.posts.build
@@ -50,6 +50,8 @@ class PostsController < ApplicationController
     unless current_user == @post.user
       flash[:alert] = "You are not authorized to perfeorm this action."
       redirect_to root_path
+    end
+  end
 
   def post_params
     params.require(:post).permit(:body)
