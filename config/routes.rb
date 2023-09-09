6f0d510f92691dd257c96845cf24190b45736fe1
Darknab <darknab@live.fr>
2023-09-09 12:57:57 +0100

    Add routes for commentd

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
     resources :friendships, only: [:index, :destroy]
   end
 
-  resources :posts
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments
+  end
+ 
 
   # Defines the root path route ("/")
   # root "articles#index"
